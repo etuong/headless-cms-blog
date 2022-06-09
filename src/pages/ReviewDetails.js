8d562506c868bd761d66b136f3bda1b186caf2b4
etuong <etuong@gmail.com>
2022-06-09 16:55:22 -0700

    Fixed CSS on blog contents

--- src/pages/ReviewDetails.js
+++ src/pages/ReviewDetails.js
@@ -17,71 +17,69 @@ export default function ReviewDetails() {
       <SiteHeader showFilter={false} />
 
       <div className="content-area">
-        <div className="page-content">
 
-          <div className="blog-post-main-image">
-            <img className="post-image img-responsive" src={`${URL}${data.attributes.image.data.attributes.url}`} />
-          </div>
-
-          <div className="blog-post-content">
-            <h1>{data.attributes.title}</h1>
-
-            <div className="entry-meta">
-              <span className="date"><i className="fa fa-fw fa-clock-o"></i>{`${new Date(data.attributes.updatedAt)}`}</span>
-              <span className="divider">|</span>
-              <span className="autor"><i className="fa fa-fw fa-user"></i>{data.attributes.author}</span>
-            </div>
-
-            <ReactMarkdown>{data.attributes.body}</ReactMarkdown>
-
-            <div className="post-comments">
-              <div className="block-title">
-                <h3>Comments (2)</h3>
-              </div>
-
-              <div className="media">
-                <div className="media-body">
-                  <div className="media-heading">
-                    <a href="#">John Doe</a> <span className="divider">|</span> <span className="light-gray">1 hour ago</span>
-                  </div>
-                  <p>Nam pellentesque laoreet augue sed facilisis. Suspendisse nec sollicitudin mauris. Integer fringilla a odio sit amet fermentum. Nunc nibh mauris, pretium eu risus et, finibus facilisis lorem.</p>
-                </div>
-
-              </div>
-
-              <div className="media">
-                <div className="media-body">
-                  <div className="media-heading">John Doe<span className="divider">|</span> <span className="light-gray">1 hour ago</span>
-                  </div>
-                  <p>Nam pellentesque laoreet augue sed facilisis. Suspendisse nec sollicitudin mauris. Integer fringilla a odio sit amet fermentum. Nunc nibh mauris, pretium eu risus et, finibus facilisis lorem.</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="post-comment-add">
-              <div className="block-title">
-                <h3>Leave a Comment</h3>
-              </div>
-              <form className="form-add-comment">
-                <div className="form-group form-group-with-icon">
-                  <i className="fa fa-user"></i>
-                  <label>Your Name</label>
-                  <input className="form-control" placeholder="" name="name" />
-                  <div className="form-control-border"></div>
-                </div>
-                <div className="form-group form-group-with-icon">
-                  <i className="fa fa-comment"></i>
-                  <label>Message</label>
-                  <textarea className="form-control" placeholder="" name="message"></textarea>
-                  <div className="form-control-border"></div>
-                </div>
-                <button className="button" type="submit">Add comment</button>
-              </form>
-            </div>
-          </div>
-
+        <div className="blog-post-main-image">
+          <img className="post-image img-responsive" src={`${URL}${data.attributes.image.data.attributes.url}`} />
         </div>
+
+        <div className="blog-post-content">
+          <h1>{data.attributes.title}</h1>
+
+          <div className="entry-meta">
+            <span className="date"><i className="fa fa-fw fa-clock-o"></i>{`${new Date(data.attributes.updatedAt)}`}</span>
+            <span className="divider">|</span>
+            <span className="autor"><i className="fa fa-fw fa-user"></i>{data.attributes.author}</span>
+          </div>
+
+          <ReactMarkdown>{data.attributes.body}</ReactMarkdown>
+
+          <div className="post-comments">
+            <div className="block-title">
+              <h3>Comments (2)</h3>
+            </div>
+
+            <div className="media">
+              <div className="media-body">
+                <div className="media-heading">John Doe<span className="divider">|</span> <span className="light-gray">1 hour ago</span>
+                </div>
+                <p>Nam pellentesque laoreet augue sed facilisis. Suspendisse nec sollicitudin mauris. Integer fringilla a odio sit amet fermentum. Nunc nibh mauris, pretium eu risus et, finibus facilisis lorem.</p>
+              </div>
+
+            </div>
+
+            <div className="media">
+              <div className="media-body">
+                <div className="media-heading">John Doe<span className="divider">|</span> <span className="light-gray">1 hour ago</span>
+                </div>
+                <p>Nam pellentesque laoreet augue sed facilisis. Suspendisse nec sollicitudin mauris. Integer fringilla a odio sit amet fermentum. Nunc nibh mauris, pretium eu risus et, finibus facilisis lorem.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="post-comment-add">
+            <div className="block-title">
+              <h3>Leave a Comment</h3>
+            </div>
+            <form className="form-add-comment">
+              <div className="form-group form-group-with-icon">
+                <i className="fa fa-user"></i>
+                <label>Your Name</label>
+                <input className="form-control" placeholder="" name="name" />
+                <div className="form-control-border"></div>
+              </div>
+              <div className="form-group form-group-with-icon">
+                <i className="fa fa-comment"></i>
+                <label>Message</label>
+                <textarea className="form-control" placeholder="" name="message"></textarea>
+                <div className="form-control-border"></div>
+              </div>
+              <button className="button" type="submit">Add comment</button>
+            </form>
+          </div>
+        </div>
+
       </div>
+
 
     </React.Fragment>
   )
