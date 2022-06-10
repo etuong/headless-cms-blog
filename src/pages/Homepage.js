b2e6bea78da9c3e652d62531251457283832fdcf
etuong <etuong@gmail.com>
2022-06-09 17:26:47 -0700

    Finish blog page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -39,29 +39,6 @@ export default function Homepage() {
             </div>
 
           </div>))}
-
-          {data.map(review => (
-          <div key={review.id} className="card">
-            <div className="card__header">
-              <img src={`${URL}${review.attributes.image.data.attributes.url}`} alt="card__image" className="card__image" width="600" />
-            </div>
-            <div className="card__body">
-              <span className="tag tag-blue">Technology</span>
-              <h4>{review.attributes.title}</h4>
-              <p>{review.attributes.body.substring(0, 200)}...</p>
-              <Link to={`/details/${review.id}`}>Read more</Link>
-            </div>
-            <div className="card__footer">
-              <div className="user">
-                <img src={`${URL}${review.attributes.avatar.data.attributes.url}`} alt="user__image" className="user__image" />
-                <div className="user__info">
-                  <h5>{review.attributes.author}</h5>
-                  <small>{getDateFromISO(review.attributes.updatedAt)}</small>
-                </div>
-              </div>
-            </div>
-
-          </div>))}
       </div>
     </React.Fragment>
   )
