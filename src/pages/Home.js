0c5100d13004b2023bdf3cad38351198eecde84b
etuong <etuong@gmail.com>
2022-07-23 11:18:57 -0700

    Bring in SASS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,33 @@
 export default function Home() {
   return (
     <div className="home">
-      <h3>
-        This web app works in conjunction with Strapi to create a full stack
-        blogging website.{" "}
-      </h3>
       <p>
-        Strapi is a headless CMS. It is useful to rapidly create artifacts and
-        assets in a content registry that can easily be pulled using RESTful API
-        or GraphQL.
+        <b>
+          This web app works in conjunction with Strapi to create a full stack
+          blogging website.
+        </b>
       </p>
+
       <p>
-        You can create blogging contents on my{" "}
+        Strapi is a headless CMS (Content Management System). It is useful to
+        rapidly create artifacts and assets in a content registry that can
+        easily be pulled using RESTful API or GraphQL.
+      </p>
+
+      <p>
+        Traditionally, we have CMS like Wordpress that requires a database to
+        store content and there's also a presentation layer of template files
+        that mix HTML with template tags.
+      </p>
+
+      <p>
+        In this type of architecture, the frontend and backend are tighly
+        coupled. Both the content and how it's presented are part of the same
+        code base (aka monolithic architecture).
+      </p>
+
+      <p>
+        You can create blogging contents on my &nbsp;
         <a
           href="https://ethan-strapi.herokuapp.com/admin/"
           target="_blank"
@@ -19,10 +35,21 @@ export default function Home() {
         >
           Strapi server
         </a>{" "}
-        and use my dummy credentials to log in{" "}
+        &nbsp;and use my dummy credentials to log in
       </p>
-      <br />
-      <pre>Email: email@email.com Password: Password1</pre>
+
+      <pre>
+        <code>
+          Email: email@email.com
+          <br />
+          Password: Password1
+        </code>
+      </pre>
+
+      <p>
+        To consume data from Strapi and blog here on this client, you'll need to
+        create contents in the admin panel under Content Manager.
+      </p>
     </div>
   );
 }
